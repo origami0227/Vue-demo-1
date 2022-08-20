707cf646a8d7de4de215d8cd29e9d29eaf47bf0c
origami <1071242996@qq.com>
2022-08-20 13:22:30 +0800

    update

---
+++ src/Demo2.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="red">
+    {{ message }}
+<!--    <button @click="add">+1</button>&lt;!&ndash;写在页面里的视图&ndash;&gt;-->
+  </div>
+</template>
+
+<script>
+export default {
+  props:['message']
+}
+//   data() {
+//     return {
+//       n: 0
+//     }
+//   },
+//   created() {
+//     console.log('出现在内存中，没有出现在页面中')
+//   },
+//   mounted() {
+//     console.log('我已出现在页面中')
+//   },
+//   updated() {
+//     console.log('更新了')
+//     console.log(this.n)
+//   },
+//   destroyed() {
+// console.log('从页面中消失了')
+//   },
+//   methods: {
+//     add() {
+//       this.n += 1
+//     },
+//   },
+// }
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -4,42 +4,25 @@
 const Vue = window.Vue
 Vue.config.productionTip = false
 //
-import Demo from './Demo.vue'
+import Demo2 from './Demo2.vue'
 
-Vue.component('Demo2', {
-    template:`
-    <div>demo2</div>   
-    `
-})
-const vm = new Vue({
-    components: {
-        Origami: Demo
+// Vue.component('Demo2', {
+//     template:`
+//     <div>demo2</div>
+//     `
+// })
+new Vue({
+    data: {
+        visible: true
     },
-    data() {
-        return {
-            n: 0,
-            array: [1, 2, 3, 4, 5, 6, 7, 8]
-        }
-    },
+    components: {Demo2},
     template: `
-      <div>
-      <div class="red">
-        {{ n }}
-      </div>
-      <button @click="add">+1</button><!--写在页面里的视图-->
-      <Origami/>
-      <Demo2/>
-      <hr>
-      {{ filter() }}<!--求数组的偶数-->
-      </div>
+        <Demo2 message="你好 props"/>
     `,
     methods: {
-        add() {
-            this.n += 1
-        },
-        filter() {
-            return this.array.filter(i => i % 2 === 0)
+        toggle() {
+            this.visible = !this.visible
         }
-    }
-})
-vm.$mount('#app')
+    },
+    // render: h => h(Demo2)
+}).$mount('#app')
